5189de5e176991575e67ff656ae447b59dbf2e06
iniarvin <iniarvinhehe@gmail.com>
2024-03-24 22:32:13 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul6_1302223136/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302223136
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String  title, int playCount)
+        {
+            Random i = new Random();
+            this.id = i.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int x)
+        {
+            this.playCount += x;
+        }
+
+        public void PrintVideoDetails()
+        {
+            System.Console.WriteLine("Video ID: " +  this.id);
+            System.Console.WriteLine("Title: " + this.title);
+            System.Console.WriteLine("Number of plays: " + this.playCount);
+        }
+    }
+}
